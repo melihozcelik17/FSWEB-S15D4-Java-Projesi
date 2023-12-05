e33652de8ab21b5e1f9e8fb1ee9315bc93774147
melihozcelik17 <melih.ozcelik17@gmail.com>
2023-12-05 23:37:30 +0300

    s15d4 tamamlandı.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +14,51 @@ public class Main {
     }
 
     public static boolean checkForPalindrome(String text) {
+        LinkedList<Character> stack=new LinkedList<>();
+        StringBuilder noPunctionString=new StringBuilder();
+        String lowerCase=text.toLowerCase();
+
+        for( int i = 0 ; i<lowerCase.length(); i ++){
+            char c=lowerCase.charAt(i);
+            if(c >= 'a' && c <= 'z' ) {
+                noPunctionString.append(c);
+                //noPunctionString.insert();
+                stack.push(c);
+
+            }
+        }
+
         return false;
     }
+
+
+    public static boolean checkForPalindormeFIFO(String text) {
+        LinkedList<Character> stack = new LinkedList<>();
+        StringBuilder queue = new StringBuilder();
+
+        String lowerCase = text.toLowerCase(Locale.ENGLISH);
+        for (int i = 0; i < lowerCase.length(); i++) {
+            char c = lowerCase.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                queue.append(c);
+                stack.push(c);
+
+            }
+        }
+
+        return false;
+    }
+        public String convertDecimalToBinary(int num){
+        final int base=2;
+            Stack digits = new Stack();
+            while (num>0){
+                digits.push(num % 2);
+                num= num / 2 ;
+            }
+            String bits = "";
+            while (!digits.isEmpty()){
+                bits+=digits.pop();
+            }
+            return bits;
+        }
 }
